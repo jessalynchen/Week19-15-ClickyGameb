c77fc1adcc93acac800e978ff8d6aa0f8d303d4a
jessalyn <35002728+jessalynchen@users.noreply.github.com>
2018-10-09 17:21:06 -0400

    fix clicks

--- src/cards.js
+++ src/cards.js
@@ -1,51 +1,19 @@
 const cards = [
     {
         id: 1,
-        image: require("./images/Bugs_Bunny.png")
+        image: require("./images/image1.jpg")
     },
     {
         id: 2,
-        image: require("./images/Captain_Planet.png")
+        image: require("./images/image2.jpg")
     },
     {
         id: 3,
-        image: require("./images/Courage.png")
+        image: require("./images/image3.jpg")
     },
     {
         id: 4,
-        image: require("./images/Dexter.png")
-    },
-    {
-        id: 5,
-        image: require("./images/Doug.gif")
-    },
-    {
-        id: 6,
-        image: require("./images/ickis.png")
-    },
-    {
-        id: 7,
-        image: require("./images/Johnny_Bravo.png")
-    },
-    {
-        id: 8,
-        image: require("./images/Mr._Krabs.png")
-    },
-    {
-        id: 9,
-        image: require("./images/Rockos.png")
-    },
-    {
-        id: 10,
-        image: require("./images/Spongebob.png")
-    },
-    {
-        id: 11,
-        image: require("./images/Squidward.png")
-    },
-    {
-        id: 12,
-        image: require("./images/Tommy_Pickles.png")
+        image: require("./images/image4.jpg")
     }
 ]
 
